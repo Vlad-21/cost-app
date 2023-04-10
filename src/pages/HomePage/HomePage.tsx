b48e6c0b3e3672ef42c612e625d3df71c85b34fc
vladkryshchuk <vladkryshchuk20@gmail.com>
2023-04-10 19:31:19 +0300

    add sass, change name and favicon, create home and login page

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './HomePage.scss';
+
+const HomePage: React.FC = () => {
+    return <h1>HomePage</h1>
+}
+
+export default HomePage;
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './LoginPage.scss';
+
+const LoginPage: React.FC = () => {
+    return <h1>LoginPAge</h1>
+}
+
+export default LoginPage;
